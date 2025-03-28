5f2ab439b4ec8b88f22a900c515067f1511b2d08
JohannesGrosseBoymann <dohecaf1219@gmail.com>
2025-03-28 16:30:51 +0100

    implement product searchByCategory

--- app/api/products/route.js
+++ app/api/products/route.js
@@ -2,12 +2,26 @@ import { NextResponse } from "next/server";
 import dbConnect from "../../../db/connect";
 import Product from "../../../db/models/Product";
 
-export async function GET() {
+export async function GET(request) {
   await dbConnect();
 
+  const { searchParams } = new URL(request.url);
+  const category = searchParams.get("category"); // Get category from query params
+
+
+  let products;
+
   try {
-    const products = await Product.find();
+    if (category !== "All products") { // Check if category is provided and not "All products"
+
+      products = await Product.find({ category: category }) // Match by category name
+
+    } else {  
+    products = await Product.find();
+    }
+
     return NextResponse.json(products, { status: 200 });
+
   } catch (error) {
     console.error("Error fetching products:", error);
     return NextResponse.json({ message: "Error fetching products" }, { status: 500 });
