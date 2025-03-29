6a5dbc7887610e184d338dbaf7c3b8b49b1747cc
JohannesGrosseBoymann <dohecaf1219@gmail.com>
2025-03-29 12:29:49 +0100

    style CategoryPage, style Filter Component

--- components/Filter.js
+++ components/Filter.js
@@ -3,7 +3,7 @@ import React from "react";
 const Filter = () => {
   return (
     <div className="mt-12 flex justify-between">
-      <div className="flex flex-wrap gap-6">
+      <div className="mt-2 mb-2 flex flex-wrap gap-6">
         <select
           name="type"
           id=""
@@ -11,7 +11,7 @@ const Filter = () => {
         >
           <option value="">Type</option>
           <option value="physical">Physical</option>
-          <option value="digital">Digital</option>
+          
         </select>
         <input
           type="text"
@@ -34,6 +34,7 @@ const Filter = () => {
           <option value="small">small</option>
           <option value="medium">medium</option>
           <option value="large">large</option>
+          <option value="xlarge">Xlarge</option>
         </select>
         <select
           name="color"
@@ -41,8 +42,11 @@ const Filter = () => {
           className="py-2 px-4 rounded-xl text-xs font-medium bg-gray-300 text-black"
         >
           <option value="">Color</option>
-          <option value="physical">Physical</option>
-          <option value="digital">Digital</option>
+          <option value="white">White</option>
+          <option value="black">Black</option>
+          <option value="red">Red</option>
+          <option value="green">Green</option>
+          <option value="purple">Purple</option>
         </select>
         <select
           name="color"
@@ -53,7 +57,8 @@ const Filter = () => {
           <option value="dresses">Dresses</option>
           <option value="bags">Bags</option>
           <option value="slippers">Slippers</option>
-          <option value="accessoiries">Accessoiries</option>
+          <option value="accessoirs">Accessoirs</option>
+          <option value="men fashion">Men fashion</option>
         </select>
         <select
           name="type"
@@ -63,17 +68,17 @@ const Filter = () => {
           <option value="">All FIlters</option>
         </select>
       </div>
-      <div>
+      <div className="mt-2">
         <select
           name="type"
           id=""
           className="py-2 px-4 rounded-xl text-xs font-medium bg-primary text-white"
         >
           <option value="">Sort By</option>
-          <option value="physical">Price (low to high)</option>
-          <option value="digital">Price (high to low)</option>
-          <option value="digital">Newest</option>
-          <option value="digital">Oldest</option>
+          <option value="asc">Price (low to high)</option>
+          <option value="dsc">Price (high to low)</option>
+          <option value="newest">Newest</option>
+          <option value="oldest">Oldest</option>
         </select>
       </div>
     </div>
